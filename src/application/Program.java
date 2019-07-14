59b31ceffb923e23143f06c053c60864c2f580a1
Alisson Youssf <youssfbr@gmail.com>
2019-07-14 10:51:52 -0300

    Classe Department criada

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+	/*
+	 * Ler os dados de um trabalhador com N contratos fornecidos pelo usuário. Depois, solicitar do usuário um mês e mostrar qual foi o salário do funcionário nesse mês.
+	 * */
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
